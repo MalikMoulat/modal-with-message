467c491753389a92cfaa05f30a39136144f92a46
MalikMoulat <malik.moulat@hotmail.fr>
2023-05-02 03:33:53 +0200

    add function on button close modal

--- dist/modal.js
+++ dist/modal.js
@@ -9,17 +9,19 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 require("./modal.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function ModalWithMessage(_ref) {
-  var message = _ref.message;
+  var setVar = _ref.setVar,
+    message = _ref.message;
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
     className: "modal-wrap"
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "modal-msg"
   }, /*#__PURE__*/_react.default.createElement("button", {
-    className: "modal-btn"
+    className: "modal-btn",
+    onClick: setVar(false)
   }, "X"), /*#__PURE__*/_react.default.createElement("p", null, message))));
 }
 ModalWithMessage.propTypes = {
-  // setVar: PropTypes.func.isRequired,
+  setVar: _propTypes.default.func.isRequired,
   message: _propTypes.default.string.isRequired
 };
 var _default = ModalWithMessage;
